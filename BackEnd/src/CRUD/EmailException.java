e669f3556f21f98e6fea31cf3393c3e670f0f6d6
Wallyson Barbosa <169079821+BRN0500W@users.noreply.github.com>
2026-05-05 22:12:41 -0300

    Add files via upload

--- BackEnd/src/CRUD/EmailException.java
+++ BackEnd/src/CRUD/EmailException.java
@@ -1,12 +1,4 @@
-
-/**
- * Exceção personalizada para erros relacionados à validação de e-mail.
- */
 public class EmailException extends Exception {
-    /**
-     * Construtor que aceita uma mensagem de erro.
-     * @param message Mensagem detalhando o erro de validação.
-     */
     public EmailException(String message) {
         super(message);
     }
--- BackEnd/src/CRUD/SenhaException.java
+++ BackEnd/src/CRUD/SenhaException.java
@@ -1,10 +1,4 @@
-
- 
 public class SenhaException extends Exception {
-    /**
-     * Construtor que aceita uma mensagem de erro.
-     * @param message Mensagem detalhando o erro de validação.
-     */
     public SenhaException(String message) {
         super(message);
     }
